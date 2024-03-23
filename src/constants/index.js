1e479493c536a30a89acb8a296fc573c32b5722e
abdel <abdelnour.rekkab@gmail.com>
2024-03-24 00:16:51 +0100

    HardSkills finis

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,12 @@ import jeuBoule from "../assets/jeuboule.jpg";
 import backpackHero from "../assets/backPack.jpeg";
 import scemantic from "../assets/scemantic.png";
 import jquery from "../assets/tech/jquery.png";
+import bank from "../assets/bank.jpeg";
+import graph from "../assets/oriented_graph.png";
+import djikstra from "../assets/djikstra.png";
+import medic from "../assets/medic.jpeg";
+import unesco from "../assets/unesco.png";
+import ihm from "../assets/ihm.jpeg";
 
 
 import {
@@ -237,7 +243,7 @@ import {
       source_code_link: "https://github.com/abdelrkb/portfolio",
     },
     {
-      name: "Jeu Boule",
+      name: "Jeu Bataille des Boules",
       description:
         "Jeu vidéo codé dans le cadre d'un projet universitaire, en python qui consistait à une bataille de surface",
       tags: [
@@ -339,4 +345,238 @@ import {
     },
   ];
 
-  export { services, technologies, experiences, testimonials, projects, hs };
+//PROJETS COMPETENCES
+const unescoPJ =     {
+  name: "Projet Unesco",
+  description:
+    "Le Projet UNESCO, réalisé en groupe de 4 persones et en partenariat avec l'UNESCO avait pour but de créer un site internet ayant pour but de promouvoir les Rives de Sein inscrit au patrimoine de l'unesco. Ce site est disponible dans trois langues différentes.",
+  tags: [
+    {
+      name: "Projet Universitaire",
+      color: "green-text-gradient",
+    },
+    {
+      name: "HTML5",
+      color: "blue-text-gradient",
+    },
+    {
+      name: "CSS3",
+      color: "pink-text-gradient",
+    },
+    {
+      name: "PHP",
+      color: "orange-text-gradient",
+    },
+    {
+      name: "SQL",
+      color: "purple-text-gradient",
+    },
+    {
+      name: "PhpMyAdmin",
+      color: "yellow-text-gradient",
+    },
+  ],
+  image: unesco,
+  source_code_link: "https://github.com/abdelrkb/UnescoRiveDeSeine",
+}
+
+const scemanticPJ = {
+  name: "Scemantic",
+  description:
+    "Jeu Vidéo sur un serveur en ligne développé dans le cadre d'un projet universitaire avec un groupe de quatre personnes",
+  tags: [
+    {
+      name: "Projet Universitaire",
+      color: "green-text-gradient",
+    },
+    {
+      name: "Java21",
+      color: "blue-text-gradient",
+    },
+    {
+      name: "C",
+      color: "purple-text-gradient",
+    },
+    {
+      name: "PHP",
+      color: "pink-text-gradient",
+    },
+    {
+      name: "JS",
+      color: "orange-text-gradient",
+    },
+    {
+      name: "Python",
+      color: "orange-text-gradient",
+    },
+    {
+      name: "React",
+      color: "orange-text-gradient",
+    },
+    {
+      name: "TypeScript",
+      color: "orange-text-gradient",
+    },
+    {
+      name: "NodeJs",
+      color: "orange-text-gradient",
+    },
+  ],
+  image: scemantic,
+  source_code_link: "https://github.com/abdelrkb/SAE_SEMANTIC",
+}
+
+const banquePJ = {
+  name: "Portail Banquaire",
+  description:
+    "Création d'un site web dynamique dédié à la gestion des paiements par cartes bancaires. \nGrâce à un backlog préétabli, j'ai maintenu un flux de travail harmonieux tout au long du développement, garantissant la qualité du projet de bout en bout.",
+  tags: [
+    {
+      name: "Projet Universitaire",
+      color: "green-text-gradient",
+    },
+    {
+      name: "PHP",
+      color: "blue-text-gradient",
+    },
+    {
+      name: "SQL",
+      color: "pink-text-gradient",
+    },
+    {
+      name: "JavaScript",
+      color: "orange-text-gradient",
+    },
+    {
+      name: "PhpMyAdmin",
+      color: "purple-text-gradient",
+    },
+  ],
+  image: bank,
+  source_code_link: "https://github.com/abdelrkb/ProjetBanque",
+}
+const exploAlgoPJ =  {
+  name: "Exploration Algorithmique",
+  description:
+    "Implémentation des algorithme sur les graphes. Ce projet offre la possibilité d'effectuer différentes transformations sur les automates. Parmi les transformations plus avancées, on trouve la minimisation, produit, ainsi que la déterminisation d'automates.",
+  tags: [
+    {
+      name: "Projet Universitaire",
+      color: "green-text-gradient",
+    },
+    {
+      name: "Python",
+      color: "blue-text-gradient",
+    },
+  ],
+  image: graph,
+  source_code_link: "https://github.com/abdelrkb/Exploration_Algorithmique",
+}
+
+const parcoursAlgoPJ = {
+  name: "Parcours Algorithmique",
+  description:
+    "Implémentation des algorithme de parcours sur les graphes. Ce projet offre la possibilité d'effectuer parcours sur les automates.",
+  tags: [
+    {
+      name: "Projet Universitaire",
+      color: "green-text-gradient",
+    },
+    {
+      name: "Python",
+      color: "blue-text-gradient",
+    },
+  ],
+  image: djikstra,
+  source_code_link: "https://github.com/abdelrkb/Automates",
+}
+
+const boulegamePJ = {
+  
+    name: "Jeu Bataille des Boules",
+    description:
+      "Jeu vidéo codé dans le cadre d'un projet universitaire, en python qui consistait à une bataille de surface",
+    tags: [
+      {
+        name: "Projet Universitaire",
+        color: "green-text-gradient",
+      },
+      {
+        name: "python",
+        color: "blue-text-gradient",
+      },
+
+    ],
+    image: jeuBoule,
+    source_code_link: "https://github.com/abdelrkb/jeuBoule",
+  }
+
+const medicPJ = {
+  name: "Centre médical",
+  description:
+    "Création d'un site web dynamique dédié à la gestion d'un centre médical. \nGrâce à un backlog préétabli, j'ai maintenu un flux de travail harmonieux tout au long du développement, garantissant la qualité du projet de bout en bout.",
+  tags: [
+    {
+      name: "Projet Universitaire",
+      color: "green-text-gradient",
+    },
+    {
+      name: "PHP",
+      color: "blue-text-gradient",
+    },
+    {
+      name: "SQL",
+      color: "pink-text-gradient",
+    },
+    {
+      name: "PhpMyAdmin",
+      color: "purple-text-gradient",
+    },
+  ],
+  image: medic,
+  source_code_link: "https://github.com/abdelrkb/CentreMedical",
+}
+
+const ihmPJ = {
+  name: "Les Visiteurs",
+  description:
+    "Création d'une maquette d'aplication de réalité virtuelle dans Paris",
+  tags: [
+    {
+      name: "Projet Universitaire",
+      color: "green-text-gradient",
+    },
+    {
+      name: "Figma",
+      color: "blue-text-gradient",
+    },
+  ],
+  image: ihm,
+  source_code_link: "",
+}
+
+const projectsRealiser = [banquePJ,unescoPJ,scemanticPJ,ihmPJ];
+
+const projectsOptimiser = [exploAlgoPJ,parcoursAlgoPJ,boulegamePJ,scemanticPJ,];
+
+const projectsAdministrer = [scemanticPJ,];
+
+const projectsGerer = [banquePJ, medicPJ,scemanticPJ,];
+
+const projectsConduire = [scemanticPJ,unescoPJ,ihmPJ];
+
+const projectsCollaborer = [unescoPJ,scemanticPJ, boulegamePJ, ihmPJ];
+
+export { 
+    services, 
+    technologies, 
+    experiences, 
+    testimonials, 
+    projects, 
+    hs, 
+    projectsRealiser, 
+    projectsOptimiser, 
+    projectsAdministrer, 
+    projectsGerer, 
+    projectsConduire, 
+    projectsCollaborer };
