b55d0a9ed12109d2ab111d90b90f06804d689499
abdelrkb <abdelnour.rekkab@gmail.com>
2025-03-29 21:50:42 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio/", 
+  base: "/portfolio/",
   build: {
     outDir: "dist",
   },
   plugins: [react()],
 })
-
